ec89ff75711c15d3fa2f8513e26f793587728fa9
Reece H. Dunn <msclrhd@gmail.com>
2013-09-28 16:20:56 +0000

    autogen.sh: don't generate an error when linking config.guess/config.sub

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR=misc
 
-ln -sv /usr/share/${CONFIG_DIR}/config.guess config.guess
-ln -sv /usr/share/${CONFIG_DIR}/config.sub config.sub
+ln -svf /usr/share/${CONFIG_DIR}/config.guess config.guess
+ln -svf /usr/share/${CONFIG_DIR}/config.sub config.sub
 
 autoheader || exit 1
