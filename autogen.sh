b8fe2219b0dabe5b1a3d74df6b724e150c29e0f8
Reece H. Dunn <msclrhd@gmail.com>
2013-10-16 11:42:51 +0100

    Use autoconf to generate configure

--- autogen.sh
+++ autogen.sh
@@ -6,3 +6,6 @@ ln -svf /usr/share/${CONFIG_DIR}/config.guess config.guess
 ln -svf /usr/share/${CONFIG_DIR}/config.sub config.sub
 
 autoheader || exit 1
+
+# NOTE: rsynth configure.ac does not work with autoconf 2.69.
+autoconf2.59 || exit 1
