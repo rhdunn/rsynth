79d849a2b8707c15abab2d0d8cb567bd480d82b7
Reece H. Dunn <msclrhd@gmail.com>
2014-01-05 17:29:15 +0000

    autogen.sh: use aclocal to create the m4 cache

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,10 @@ CONFIG_DIR=misc
 ln -svf /usr/share/${CONFIG_DIR}/config.guess config.guess
 ln -svf /usr/share/${CONFIG_DIR}/config.sub config.sub
 
+mkdir -p m4
+
+aclocal -I m4 || exit 1
+
 autoheader || exit 1
 
 # NOTE: rsynth configure.ac does not work with autoconf 2.69.
