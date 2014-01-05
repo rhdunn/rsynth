9e2691742153f4dcd7a0f8ad6630dcf997ac2f85
Reece H. Dunn <msclrhd@gmail.com>
2014-01-05 20:41:43 +0000

    autotools: use automake to build the rsynth applications

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-CONFIG_DIR=misc
-
-ln -svf /usr/share/${CONFIG_DIR}/config.guess config.guess
-ln -svf /usr/share/${CONFIG_DIR}/config.sub config.sub
-
 mkdir -p m4
 
 aclocal -I m4 || exit 1
 
 autoheader || exit 1
+automake --add-missing || exit 1
 autoconf || exit 1
